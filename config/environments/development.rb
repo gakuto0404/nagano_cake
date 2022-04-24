77872d452f0786fbe3ca1d53bc4afc6188fe1f34
EC2 Default User <gakuto404@gmail.com>
2022-04-24 14:12:05 +0000

    [finsh]すべてのmodel追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "56e82b4b126f49889b243e028c0cf2a4.vfs.cloud9.us-east-1.amazonaws.com"
 end

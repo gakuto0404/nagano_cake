09e3bd3ffed2c641c5e6363a59e246e86104bdf8
EC2 Default User <gakuto404@gmail.com>
2022-04-26 15:27:57 +0000

    [start]genre画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,33 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genres = Genre.all
+    @genre = save
+    #redirect_to admins_genres_index_path
   end
 
   def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      @genres = Genre.all
+      #redirect_to admins_genres_path
+    else
+      #render edit
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end

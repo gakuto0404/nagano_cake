5a6a9768933546ffeaf619bf268bb96f174ea89b
EC2 Default User <gakuto404@gmail.com>
2022-04-30 18:25:13 +0000

    [on hold]customers保留

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,16 +1,25 @@
-class Admin::CustomersController < ApplicationController
+class Admin::CustomersController < ApplicationController　#顧客側をやらないと進めない気がする
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
-  
+
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
   end
-  
+
   private
 
   def customer_params

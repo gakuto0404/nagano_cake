8d35c3e513dfae3535735337c302be1eafbe53d7
EC2 Default User <gakuto404@gmail.com>
2022-05-09 15:54:00 +0000

    [start]注文情報入力画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,11 +7,19 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    #@item = Item.find(cart_item_params[:item_id])
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to public_cart_items_path
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_item.item_id = params[:cart_item][:item_id]
+    current_cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    #選択した商品が既にカート内に存在するの判別
+    if current_cart_item.present?
+      current_cart_item.amount += params[:cart_item][:amount].to_i   #カート内の個数をフォームから送られた個数分追加する
+      current_cart_item.save
+      redirect_to public_cart_items_path
+    else
+      #選択した商品がカート内に存在しない場合
+      @cart_item.save
+      redirect_to public_cart_items_path
+    end
   end
 
   def update
